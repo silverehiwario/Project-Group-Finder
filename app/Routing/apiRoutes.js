70e167af44f52c7f525074d4858220e769489a55
silverehiwario <silverehiwario@gmail.com>
2017-10-07 23:40:06 -0500

    updated connection

--- app/Routing/apiRoutes.js
+++ app/Routing/apiRoutes.js
@@ -8,7 +8,13 @@
 
 
 
-
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
 
 
 // ==============================================================================
